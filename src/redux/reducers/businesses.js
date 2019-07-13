cc7e7ea92d5120f8f9627a7bcebaab3ee755f4e5
Zhenyi Li <zhenyili@Zhenyis-MacBook-Pro.local>
2019-07-12 23:37:50 -0400

    update

--- src/redux/reducers/businesses.js
+++ src/redux/reducers/businesses.js
@@ -1,10 +1,10 @@
-import {BUSINESSES_RECEIVED} from '../constants/index';
-import {Yelp} from '../../util/Yelp';
+import { BUSINESSES_RECEIVED } from '../constants/index';
+import { Yelp } from '../../util/Yelp';
 
-const businessesReducer = (state = {}, action) => {
+const businessesReducer = (state = [], action) => {
   switch (action.type) {
     case BUSINESSES_RECEIVED:
-      return action.payload;
+      return Object.assign([],action.payload);
     default:
       return state;
   }
--- src/redux/reducers/searchParams.js
+++ src/redux/reducers/searchParams.js
@@ -1,18 +1,18 @@
 import { UPDATE_SEARCH_PARAMS } from "../constants";
 
 const initialState = {
-    term:"",
-    location:"",
+    term: "",
+    location: "",
     sortBy: "best_match"
 };
 
-const searchParamsReducer = (state=initialState,action) => {
-    switch(action.type){
+const searchParamsReducer = (state = initialState, action) => {
+    switch (action.type) {
         case UPDATE_SEARCH_PARAMS:
-            return Object.assign({},action.payload);
+            return Object.assign({}, state, action.payload);
         default:
             return state;
     };
 };
 
-export {searchParamsReducer};
+export { searchParamsReducer };
